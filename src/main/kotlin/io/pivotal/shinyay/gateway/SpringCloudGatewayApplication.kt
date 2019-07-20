fafd4c4d61edd7acf7ec1f1842f99fd3bd69e00a
shinyay <shinya.com@gmail.com>
2019-07-20 17:10:26 +0900

    Add RouteLocator implementation to forward another uri

--- src/main/kotlin/io/pivotal/shinyay/gateway/SpringCloudGatewayApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/gateway/SpringCloudGatewayApplication.kt
@@ -2,9 +2,23 @@ package io.pivotal.shinyay.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringCloudGatewayApplication
+class SpringCloudGatewayApplication {
+
+	@Bean
+	fun routeLocator(builder: RouteLocatorBuilder): RouteLocator =
+			builder.routes()
+					.route { r ->
+						r.path("/get")
+								.filters { f -> f.addResponseHeader("Hello", "Gateway") }
+								.uri("http://httpbin.org:80")
+					}
+					.build()
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudGatewayApplication>(*args)
