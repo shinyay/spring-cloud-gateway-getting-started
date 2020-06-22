ae14576ed6f2de94a07aff8bf20e5428a33cfa24
shinyay <shinya.com@gmail.com>
2020-06-23 07:43:23 +0900

    Add Predicate for Method

--- src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
 
 @SpringBootApplication
 class SpringCloudGatewayGsApplication {
@@ -13,7 +14,7 @@ class SpringCloudGatewayGsApplication {
 	fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? = builder.routes()
 			.route { r -> r.path("/search")
 					.uri("https://www.google.co.jp") }
-			.route { r -> r.path("/get")
+			.route { r -> r.path("/get").and().method(HttpMethod.GET)
 					.filters { f -> f.addRequestHeader("Hello", "World") }
 					.uri("http://httpbin.org") }
 			.build()
