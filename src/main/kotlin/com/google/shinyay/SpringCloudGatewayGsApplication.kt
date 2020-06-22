fd9400477e1fc87cebf3b7e3feb407b4d4127e3d
shinyay <shinya.com@gmail.com>
2020-06-22 23:29:35 +0900

    Add route for Google Search

--- src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
@@ -2,9 +2,18 @@ package com.google.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringCloudGatewayGsApplication
+class SpringCloudGatewayGsApplication {
+
+	@Bean
+	fun routeLocator(builder: RouteLocatorBuilder): RouteLocator? = builder.routes()
+			.route { r -> r.path("/search").uri("https://www.google.co.jp") }
+			.build()
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudGatewayGsApplication>(*args)
