283bf0b4923a75d6eee4e6ca327bdbba24b8f212
shinyay <shinya.com@gmail.com>
2020-06-23 22:12:59 +0900

    Add Predicate - before/after/between

--- src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpMethod
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 @SpringBootApplication
 class SpringCloudGatewayGsApplication {
@@ -13,6 +15,16 @@ class SpringCloudGatewayGsApplication {
 	@Bean
 	fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? = builder.routes()
 			.route { r -> r.path("/search")
+					.uri("https://www.google.co.jp") }
+			.route { r -> r.path("/may")
+					.and()
+					.before(ZonedDateTime.of(2020,6,1,0,0,0,0, ZoneId.systemDefault()))
+					.uri("https://www.google.co.jp") }
+			.route { r -> r.path("/june")
+					.and()
+					.between(
+							ZonedDateTime.of(2020,6,1,0,0,0,0, ZoneId.systemDefault()),
+							ZonedDateTime.of(2020,6,30,0,0,0,0, ZoneId.systemDefault()))
 					.uri("https://www.google.co.jp") }
 			.route { r -> r.path("/get").and().method(HttpMethod.GET)
 					.filters { f -> f.addRequestHeader("Hello", "World") }
