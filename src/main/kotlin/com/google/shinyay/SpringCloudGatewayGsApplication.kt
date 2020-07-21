b59a5a875e8384f406902533c8af742cbb16e1f1
shinyay <shinya.com@gmail.com>
2020-07-21 22:12:47 +0900

    Add enable DiscoveryClient for Eureka Server

--- src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringCloudGatewayGsApplication.kt
@@ -2,6 +2,7 @@ package com.google.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
@@ -10,6 +11,7 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class SpringCloudGatewayGsApplication {
 
 	@Bean
